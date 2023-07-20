07672138930de0baf135865261604ddad5f36b1a
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-07-20 13:08:43 +0300

    form

--- src/js/script.js
+++ src/js/script.js
@@ -1,95 +1,47 @@
+//Фунция модального окна и удаления кнопки после нажатия
+function modal(){
+    let btn = document.querySelector('.btns'),
+    modal = document.querySelector('.modal'),
+    close = document.querySelector('.popup-close');
 
-// modal
-const modals = () => {
-    let btnPressed = false;
-    //Передача селекторов и получение переменных
-    function bindModal(triggerSelector, modalSelector, closeSelector,destroy = false) {
-        const trigger = document.querySelectorAll(triggerSelector),
-              modal = document.querySelector(modalSelector),
-              close = document.querySelector(closeSelector),
-              window = document.querySelectorAll('[data-modal]'),
-              scroll = calcScroll();
+    btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    btn.style.display = 'none';      
+    document.body.style.overflow = 'hidden';
+    
+});
 
-        // Передача в функцию несколько элементов и вешается обработчик
-        trigger.forEach(item => {
-            item.addEventListener('click', (e) => {
-                if (e.target) {
-                    e.preventDefault();
-                } 
-                btnPressed = true;
+    close.addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.style.display = 'none';
+    btn.style.display = 'block';   
+    document.body.style.overflow = '';
+    window.location.reload();
+    
+});
 
-                if(destroy){
-                    item.remove();
-                }
-                //закрываются все модальные окна
-                window.forEach(item =>{
-                    item.style.display = "none";
-                    item.classList.add('animated', 'fadeIn');
-                })               
-                modal.style.display = "block";
-                document.body.style.overflow = "hidden";
-                document.body.style.marginRight = '${scroll}px';
-            });
-         });
-
-         // Закрытие modal
-        close.addEventListener('click', () => {
-            //закрываются все модальные окна
-            window.forEach(item =>{
-                item.style.display = "none";
-            })
-            modal.style.display = "none";
-            document.body.style.overflow = "";
-            document.body.style.marginRight = '0px';
-        });
-
-        // Закрытие modal при клике вне модалного окна
-        modal.addEventListener('click', (e) => {
-            if (e.target === modal) {
-                //закрываются все модальные окна
-                window.forEach(item =>{
-                    item.style.display = "none";
-                })
-                modal.style.display = "none";
-                document.body.style.overflow = ""; 
-                document.body.style.marginRight = '0px';
+}
+modal();
+ 
+// Функция всплытия модального окна через промежуток времени
+function showModalByTime(selector, time) {
+    setTimeout(function() {
+        let display;
+        let btn = document.querySelector('.btns')
+        document.querySelectorAll('[data-modal]').forEach(item => {
+            if (getComputedStyle(item).display !== 'none') {
+                display = "block";
             }
         });
-    }
-    
-    // Функция подсчитывает расстояние в px при открытиии модального окна и появления скролла
-    function calcScroll(){
-        let div =document.createElement('div');
-        div.style.width = '50px';
-        div.style.height = '50px';
-        div.style.overflowY = 'scroll';
-        div.style.visibility ='hidden';
-
-        document.body.appendChild(div);
-        // Вычисление размера прокрутки
-        let scrollWidth = div.offsetWidth - div.clientWidth;
-        div.remove();
-
-        return scrollWidth;
-    }
-    // Функция сколько пользователь пролисталл
-    
-    function openByScroll(selector) {
-        window.addEventListener('scroll', () => {
-            let scrollHeight = Math.max(document.documentElement.scrollHeight, document.body.scrollHeight);
-
-            if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight >= scrollHeight)) {
-                document.querySelector(selector).click();
-            }
-        });
-    }
-
-    // Вызов функций
-    bindModal('.btns', '.popup', '.popup .popup_close');
-   
-    
-};
-modals();
+        btn.style.display = 'none';
+        if (!display) {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = "hidden";
+            document.body.style.marginRight = '${scroll}px';
+        }
+    }, time);
+}
+showModalByTime('.modal', 7000);
 
 
 
