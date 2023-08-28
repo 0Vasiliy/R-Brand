e158b25150b575febbbf810ff9094ca630162426
Козин-Поздняков Василий <vasiliy_pit@mail.ru>
2023-08-28 14:07:09 +0300

    popup

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,4 +1,4 @@
-//Фунция модального окна и удаления кнопки после нажатия
+// Фунция модального окна и удаления кнопки после нажатия
 function modal(){
     let btn = document.querySelector('.btns'),
     modal = document.querySelector('.modal'),
@@ -10,7 +10,6 @@ function modal(){
     document.body.style.overflow = 'hidden';
     
 });
-
     close.addEventListener('click', function(e) {
     e.preventDefault();
     modal.style.display = 'none';
@@ -22,8 +21,8 @@ function modal(){
 
 }
 modal();
- 
-// Функция всплытия модального окна через промежуток времени
+
+//  Функция всплытия модального окна через промежуток времени
 function showModalByTime(selector, time) {
     setTimeout(function() {
         let display;
@@ -41,7 +40,28 @@ function showModalByTime(selector, time) {
         }
     }, time);
 }
-showModalByTime('.modal', 7000);
+showModalByTime('.modal', 3000);
 
 
+// showModalByTime('.modal', 7000);
+// setTimeout(function() {
+//   document.querySelector('.btns').click();
+// }, 1000);
+
+
+
+// Validation
+let = forms.document.querySelectorAll('.needs-validation');
+Array.prototype.slice.call(forms).forEach(function(form)
+{
+    form.addEventListener('submit', function (event) 
+      {
+        if(!form.checkValidity())
+        {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add("was-validated")
+    },false );  
+  } );
 
